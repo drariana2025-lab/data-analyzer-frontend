893dfe0cde1d6c82760660d19341020d28ea1b57
brodovskayariana <drariana2025@gmail.com>
2026-03-31 21:52:49 +0500

    New rtpository

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',  // ← это важно для Vercel
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    host: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
